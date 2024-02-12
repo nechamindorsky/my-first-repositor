51d73262fe33c904bfe7e3d80f9aa4f018856b9e
unknown <you@example.com>
2024-02-12 14:21:50 +0200

    href

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,9 @@ function App() {
       <div>
           <img src={viteLogo} className="logo" alt="Vite logo" />
       
+        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-      
+        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
