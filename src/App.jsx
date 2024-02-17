be2ed53e829758c142882d86707e8f53af0a7748
unknown <you@example.com>
2024-02-18 00:19:46 +0200

    ilove git

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <h1>i love git</h1>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
